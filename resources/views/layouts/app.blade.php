f661161ddef2c05618924b038c0417b5a5358185
Uzair-Vibrant <your.email@example.com>
2025-09-01 12:56:41 -0700

    All Pages Title Set.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
 
         {{ $title }}
     </title>
+
     <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/favicon.png') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" />
